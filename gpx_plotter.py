47987a6336d804a4f3528386d0d77ba8e255ea54
Yozpoz64 <samuel.kolston@gmail.com>
2021-08-30 13:53:59 +1200

    initial commit

---
+++ gpx_plotter.py
@@ -0,0 +1,13 @@
+'''
+GPX Plotter
+
+plots GPX files on to a folium/leaflet map
+read full readme on GitHub repo
+
+Created by: Sophie Kolston
+'''
+
+class web_mapper():
+    def __init__(self):
+        pass
+
